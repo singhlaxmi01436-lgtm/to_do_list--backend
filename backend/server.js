3326ee80ed95cfcffdc124a2c8064bec4e604ccd
ayushman <singhlaxmi01436@gmail.com>
2026-07-06 23:54:54 +0530

    Initial commit - To-Do List project with 3 versions

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static(__dirname)); // serve index.html
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/todolist", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const TaskSchema = new mongoose.Schema({
+  text: String,
+  done: Boolean
+});
+const Task = mongoose.model("Task", TaskSchema);
+
+// Routes
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(task);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
+});
+
+app.listen(3000, () => console.log("Server running on http://localhost:3000"));
